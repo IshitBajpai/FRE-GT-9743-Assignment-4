be308b8ab8175b425dcce083187e28edc9c1f271
unknown <ishit20380@iiitd.ac.in>
2026-02-26 16:03:16 -0500

    Done HW4 v2

--- fixedincomelib/product/product_display_visitor.py
+++ fixedincomelib/product/product_display_visitor.py
@@ -29,63 +29,67 @@ class ProductDisplayVisitor(ProductVisitor):
     # ProductBulletCashflow
     @visit.register
     def _(self, product: ProductBulletCashflow):
-        self.nvps_ = []
-        self.nvps_.append(("Product Type",     product._product_type))
-        self.nvps_.append(("Currency",         product.currency_.value_str))
-        self.nvps_.append(("Notional",         product.notional_))
-        self.nvps_.append(("Long or Short",    product.long_or_short_.to_string().upper()))
-        self.nvps_.append(("Termination Date", product.last_date_.ISO()))
-        self.nvps_.append(("Payment Date",     product.paymnet_date_.ISO())) 
+        self.nvps_ = [
+            ("Product Type",     product._product_type),
+            ("Currency",         product.currency_.value_str),
+            ("Notional",         product.notional_),
+            ("Long or Short",    product.long_or_short_.to_string().upper()),
+            ("Termination Date", product.last_date_.ISO()),
+            ("Payment Date",     product.paymnet_date_.ISO()),  # note: typo in source
+        ]
 
     # ProductFixedAccrued
     @visit.register
     def _(self, product: ProductFixedAccrued):
-        self.nvps_ = []
-        self.nvps_.append(("Product Type",            product._product_type))
-        self.nvps_.append(("Currency",                product.currency_.value_str))
-        self.nvps_.append(("Notional",                product.notional_))
-        self.nvps_.append(("Long or Short",           product.long_or_short_.to_string().upper()))
-        self.nvps_.append(("Effective Date",          product.effective_date_.ISO()))
-        self.nvps_.append(("Termination Date",        product.termination_date_.ISO()))
-        self.nvps_.append(("Accrual Basis",           product.accrual_basis_.value_str))
-        self.nvps_.append(("Accrued",                 product.accrued_))
-        self.nvps_.append(("Payment Date",            product.paymnet_date_.ISO())) 
-        self.nvps_.append(("Business Day Convention", product.business_day_convention_.value_str))
-        self.nvps_.append(("Holiday Convention",      product.holiday_convention_.value_str))
+        self.nvps_ = [
+            ("Product Type",            product._product_type),
+            ("Currency",                product.currency_.value_str),
+            ("Notional",                product.notional_),
+            ("Long or Short",           product.long_or_short_.to_string().upper()),
+            ("Effective Date",          product.effective_date_.ISO()),
+            ("Termination Date",        product.termination_date_.ISO()),
+            ("Accrual Basis",           product.accrual_basis_.value_str),
+            ("Accrued",                 product.accrued_),
+            ("Payment Date",            product.paymnet_date_.ISO()),  # note: typo in source
+            ("Business Day Convention", product.business_day_convention_.value_str),
+            ("Holiday Convention",      product.holiday_convention_.value_str),
+        ]
 
     # ProductOvernightIndexCashflow
     @visit.register
     def _(self, product: ProductOvernightIndexCashflow):
-        self.nvps_ = []
-        self.nvps_.append(("Product Type",       product._product_type))
-        self.nvps_.append(("Overnight Index",    product.on_index_str_))
-        self.nvps_.append(("Currency",           product.currency_.value_str))
-        self.nvps_.append(("Notional",           product.notional_))
-        self.nvps_.append(("Long or Short",      product.long_or_short_.to_string().upper()))
-        self.nvps_.append(("Effective Date",     product.effective_date_.ISO()))
-        self.nvps_.append(("Termination Date",   product.termination_date_.ISO()))
-        self.nvps_.append(("Compounding Method", product.compounding_method_.to_string().upper()))
-        self.nvps_.append(("Spread",             product.spread_))
-        self.nvps_.append(("Payment Date",       product.paymentDate_.ISO())) 
+        self.nvps_ = [
+            ("Product Type",       product._product_type),
+            ("Overnight Index",    product.on_index_str_),
+            ("Currency",           product.currency_.value_str),
+            ("Notional",           product.notional_),
+            ("Long or Short",      product.long_or_short_.to_string().upper()),
+            ("Effective Date",     product.effective_date_.ISO()),
+            ("Termination Date",   product.termination_date_.ISO()),
+            ("Compounding Method", product.compounding_method_.to_string().upper()),
+            ("Spread",             product.spread_),
+            ("Payment Date",       product.paymentDate_.ISO()),  # note: camelCase in source
+        ]
 
     # ProductRFRSwap
     @visit.register
     def _(self, product: ProductRFRSwap):
-        self.nvps_ = []
-        self.nvps_.append(("Product Type",                product._product_type))
-        self.nvps_.append(("Overnight Index",             product.on_index_str_))
-        self.nvps_.append(("Currency",                    product.currency_.value_str))
-        self.nvps_.append(("Notional",                    product.notional_))
-        self.nvps_.append(("Long or Short",               product.long_or_short_.to_string().upper()))
-        self.nvps_.append(("Pay or Receive",              product.pay_or_rec_.to_string().upper()))
-        self.nvps_.append(("Effective Date",              product.effective_date_.ISO()))
-        self.nvps_.append(("Termination Date",            product.termination_date_.ISO()))
-        self.nvps_.append(("Fixed Rate",                  product.fixed_rate_))
-        self.nvps_.append(("Fixed Leg Accrual Period",    str(product.accrual_period_)))
-        self.nvps_.append(("Fixed Leg Accrual Basis",     product.accrual_basis_.value_str))
-        self.nvps_.append(("Floating Leg Accrual Period", str(product.floating_leg_accrual_period_)))
-        self.nvps_.append(("Compounding Method",          product.compounding_method_.to_string().upper()))
-        self.nvps_.append(("Spread",                      product.spread_))
-        self.nvps_.append(("Pay Offset",                  str(product.pay_offset_)))
-        self.nvps_.append(("Business Day Convention",     product.pay_business_day_convention_.value_str))
-        self.nvps_.append(("Holiday Convention",          product.pay_holiday_convention_.value_str))
+        self.nvps_ = [
+            ("Product Type",                product._product_type),
+            ("Overnight Index",             product.on_index_str_),
+            ("Currency",                    product.currency_.value_str),
+            ("Notional",                    product.notional_),
+            ("Long or Short",               product.long_or_short_.to_string().upper()),
+            ("Pay or Receive",              product.pay_or_rec_.to_string().upper()),
+            ("Effective Date",              product.effective_date_.ISO()),
+            ("Termination Date",            product.termination_date_.ISO()),
+            ("Fixed Rate",                  product.fixed_rate_),
+            ("Fixed Leg Accrual Period",    str(product.accrual_period_)),
+            ("Fixed Leg Accrual Basis",     product.accrual_basis_.value_str),
+            ("Floating Leg Accrual Period", str(product.floating_leg_accrual_period_)),
+            ("Compounding Method",          product.compounding_method_.to_string().upper()),
+            ("Spread",                      product.spread_),
+            ("Pay Offset",                  str(product.pay_offset_)),
+            ("Business Day Convention",     product.pay_business_day_convention_.value_str),
+            ("Holiday Convention",          product.pay_holiday_convention_.value_str),
+        ]
